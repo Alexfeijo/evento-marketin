650d280dfe17814b3cad63a55a571481b6085c88
Alexandro Feijó <desenvolvimento@cenazero.com>
2017-09-26 18:27:54 -0300

    alteracoes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,26 +6,26 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
 
-    <title>Talks: Inbound Marketing | Design Thinking | Business</title>
+    <title>MKT Improvável: Inbound Marketing onde ninguém imagina</title>
 
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_16x16.png')}}" sizes="16x16" />
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_32x32.png')}}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="img/favicon_16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="img/favicon_32x32.png" sizes="32x32" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <meta name="description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado" />
+    <meta name="description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias" />
 
     <meta property="og:url" content="index.html" />
     <meta property="og:type" content="website" />
-    <meta property="og:title" content="Talks: Inbound Marketing | Design Thinking | Business" />
-    <meta property="og:description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado" />
-    <meta property="og:image" content="img/brand.png" />
+    <meta property="og:title" content="MKT Improvável: Inbound Marketing onde ninguém imagina" />
+    <meta property="og:description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias" />
+    <meta property="og:image" content="img/cover_FB.jpg" />
 
     <meta name="twitter:card" content="summary">
     <meta name="twitter:url" content="index.html">
-    <meta name="twitter:title" content="Talks: Inbound Marketing | Design Thinking | Business">
-    <meta name="twitter:description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado">
-    <meta name="twitter:image" content="img/brand.png">
+    <meta name="twitter:title" content="MKT Improvável: Inbound Marketing onde ninguém imagina">
+    <meta name="twitter:description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias">
+    <meta name="twitter:image" content="img/cover_FB.jpg">
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
@@ -49,85 +49,79 @@
     <header class="jumbotron cover">
         <div class="row">
             <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-lg-6 col-md-6 col-sm-6 col-xs-12 title">
-                <h1>Inbound Marketing</h1>
-                <h1>Design Thinking</h1>
-                <h1>Business</h1>
-                <p>Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado</p>
+                <h1>MKT Improvável</h1>
+                <h3>Inbound Marketing onde ninguém imagina</h3>
+                <p>&nbsp;</p>
+                <p>Como ONGs, universidades e governos estão praticando <b>Inbound Marketing</b> para atrair pessoas e fechar parcerias</p>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 date-location">
                 <h3>27 Setembro</h3>
-                <h3>19h - 22h</h3>
+                <h3>19h - 21h</h3>
                 <h3>Florianópolis</h3>
             </div>
         </div>
         <div class="row cover-cta">
-            <a class="btn btn-primary btn-lg text-uppercase ">Garanta sua Vaga</a>
+            <a class="btn btn-primary btn-lg text-uppercase" href="https://www.sympla.com.br/mkt-improvavel-inbound-marketing-onde-ninguem-imagina__193893">Garanta sua Vaga</a>
         </div>
     </header>
     
     <main>
         <section class="container content-talks">
-            <h2 class="row text-center text-uppercase">Talks</h2>
+            <h2 class="row text-center text-uppercase">Quem vem?</h2>
             <p>&nbsp;</p>
             <div class="row">
                 <div class="col-md-offset-2 col-md-8 card-talk">
                     <div class="profile text-center">
-                        <img class="img-circle" src="http://via.placeholder.com/80x80"/>
-                        <p>Sobre o Palestrante</p>
+                        <img class="img-circle" src="{{ asset('assets/img/michelle.png')}}"/>
+                        <p class="speaker-name">Michelle Rocha<br />Politize!</p>
                     </div>
                     <div class="content">
-                        <h4>Título da Palestra: com subtítulo talvez bem grande</h4>
-                        <p>Nome Sobrenome</p>
-                        <p>Texto resumo da palestra, apelativo, okay? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <h4>Atração de Parceiros por meio de Inbound Marketing na Politize!</h4>
+                        <p class="speaker-name">Michelle Rocha de Moura, Designer Gráfica na Politize!</p>
+                        <p>A Politize! é uma rede de pessoas e organizações comprometidas com a ideia de levar educação política para cidadãos de todo o Brasil. A aplicação estratégica de Inbound Marketing nos canais de comunicação da Politize! é fundamental para a atrair e manter parceiros apoiadores. Vamos falar um pouco sobre como foi estruturar estas práticas e o que alcançamos até então.</p>
                     </div>
                 </div>
                 <div class="col-md-offset-2 col-md-8 card-talk">
                     <div class="profile text-center">
-                        <img class="img-circle" src="http://via.placeholder.com/80x80"/>
-                        <p>Sobre o Palestrante</p>
+                        <img class="img-circle" src="{{ asset('assets/img/profile_pic.png')}}"/>
                     </div>
                     <div class="content">
-                        <h4>Título da Palestra: com subtítulo talvez bem grande</h4>
-                        <p>Nome Sobrenome</p>
-                        <p>Texto resumo da palestra, apelativo, okay? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <h4>A definir</h4>
                     </div>
                 </div>
             </div>            
         </section>
 
         <section class="container-fluid content-testimony">
+            <h2 class="row text-center text-uppercase">Sobre</h2>
             <div class="row">
-                <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 col-xs-12">
-                    <blockquote>
-                            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat.
-                            <p class="text-right"><i>Nome Sobrenome</i></p>
-                    </blockquote>
+                <div class="col-sm-4 col-sm-offset-2 col-md-4 col-md-offset-2 col-lg-4 col-lg-offset-2 col-xs-12">
+                    <p>Convidamos a Politize! para conversar sobre como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias.
+                    </p>
+                    <p>O Inbound Marketing ou Marketing Digital tem atraído empresas de todos os tamanhos como estratégia para conversar com potenciais clientes e conseguir, ao mesmo tempo, maior assertividade e otimização de recursos no momento de vender seus produtos e serviços. Quem já não ouviu falar em funil de vendas? Em landing pages e ebooks para captar leads???</p>
+                </div>
+                <div class="col-sm-4 col-md-4 col-lg-4 col-xs-12">
+                    <p>Para empresas que visam lucro é mais do que natural praticar Inbound Marketing. Será que ONGs conseguem aplicar os mesmo métodos, as mesmas práticas? Com que objetivo? Convidamos a Michelle Rocha, designer gráfica na Politize!, para compartilhar experiências na utilização de marketing digital de conteúdo nos canais de comunicação deles.</p>
                 </div>
             </div>
         </section>
 
         <section class="container content-partners">
-            <h2 class="row text-center text-uppercase">Patrocinadores e Parceiros</h2>
+            <h2 class="row text-center text-uppercase">Parceiros e Apoiadores</h2>
 
             <p>&nbsp;</p>
             
             <div class="row">
-                <div class="col-lg-offset-3 col-lg-2 col-md-offset-3 col-md-2 col-sm-12 text-center">
+                <div class="col-lg-offset-3 col-lg-3 col-md-offset-3 col-md-3 col-sm-12 text-center">
                     <div class="profile">
-                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
+                        <a href="http://www.politize.com.br/" target="_blank"><img class="img-responsive img-thumbnail" src="{{ asset('assets/img/logotipo-header-politize1_small.png')}}"/></a>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 text-center">
+                <div class="col-lg-3 col-md-3 col-sm-12 text-center">
                     <div class="profile">
-                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
+                        <a href="https://www.vilaj.com.br/" target="_blank"><img class="img-responsive img-thumbnail" src="{{ asset('assets/img/vilaj_brand.png')}}"/></a>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 text-center">
-                    <div class="profile">
-                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
-                    </div>
-                </div>
-                
+                </div>                
             </div>        
         </section>
 
@@ -135,15 +129,15 @@
             <div class="container">
             <div class="row">
                 <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8 col-xs-12">
-                    <h2>Não perca essa oportunidade</h2>
+                    <h2>Não perca essa oportunidade!</h2>
                     <p>
-                        Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Cras ultricies ligula sed magna dictum porta.
+                        Conheça experiências inovadoras na aplicação de <b>Inbound Marketing</b> em iniciativas sem fins lucrativos.
                     </p>
                 </div>
             </div>
             <div class="row text-center">
                 <div class="col-md-12">
-                    <a href="https://www.sympla.com.br/talks-inbound-marketing--ux-design--business__193893" target="_blank" class="btn btn-primary btn-lg btn-ticket text-uppercase">Garanta seu ingresso</a>
+                    <a href="https://www.sympla.com.br/mkt-improvavel-inbound-marketing-onde-ninguem-imagina__193893" target="_blank" class="btn btn-primary btn-lg btn-ticket text-uppercase">Garanta seu ingresso</a>
                 </div>
             </div>
             </div>
@@ -152,11 +146,10 @@
 
     <footer>
         <div class="container">
-            <p>&nbsp;</p>
             <div class="row contact">
                 <p>Dúvidas? Entre em contato conosco!</p>
-                <p>email@email.com</p>
-                <p>(48) 9 9999 9999</p>
+                <p>andrea.bonette.ferrari@gmail.com</p>
+                <p>(48) 9 9646 5382</p>
             </div>
         </div>
     </footer>
