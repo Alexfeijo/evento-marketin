368e26f68bb4ecc82af3edb9dca244658a9d1cc8
Alexandro Feijó <desenvolvimento@cenazero.com>
2017-09-26 10:06:56 -0300

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+
+    <title>Talks: Inbound Marketing | Design Thinking | Business</title>
+
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_16x16.png')}}" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_32x32.png')}}" sizes="32x32" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <meta name="description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado" />
+
+    <meta property="og:url" content="index.html" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Talks: Inbound Marketing | Design Thinking | Business" />
+    <meta property="og:description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado" />
+    <meta property="og:image" content="img/brand.png" />
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:url" content="index.html">
+    <meta name="twitter:title" content="Talks: Inbound Marketing | Design Thinking | Business">
+    <meta name="twitter:description" content="Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado">
+    <meta name="twitter:image" content="img/brand.png">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script src="{{ asset('assets/js/script.js')}}"></script>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,800,800i|Open+Sans:400,400i,700,700i" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
+
+</head>
+
+<body>
+    <header class="jumbotron cover">
+        <div class="row">
+            <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-lg-6 col-md-6 col-sm-6 col-xs-12 title">
+                <h1>Inbound Marketing</h1>
+                <h1>Design Thinking</h1>
+                <h1>Business</h1>
+                <p>Conheça os fundamentos das três áreas mais importantes para o sucesso de um produto com experts do mercado</p>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 date-location">
+                <h3>27 Setembro</h3>
+                <h3>19h - 22h</h3>
+                <h3>Florianópolis</h3>
+            </div>
+        </div>
+        <div class="row cover-cta">
+            <a class="btn btn-primary btn-lg text-uppercase ">Garanta sua Vaga</a>
+        </div>
+    </header>
+    
+    <main>
+        <section class="container content-talks">
+            <h2 class="row text-center text-uppercase">Talks</h2>
+            <p>&nbsp;</p>
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8 card-talk">
+                    <div class="profile text-center">
+                        <img class="img-circle" src="http://via.placeholder.com/80x80"/>
+                        <p>Sobre o Palestrante</p>
+                    </div>
+                    <div class="content">
+                        <h4>Título da Palestra: com subtítulo talvez bem grande</h4>
+                        <p>Nome Sobrenome</p>
+                        <p>Texto resumo da palestra, apelativo, okay? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+                </div>
+                <div class="col-md-offset-2 col-md-8 card-talk">
+                    <div class="profile text-center">
+                        <img class="img-circle" src="http://via.placeholder.com/80x80"/>
+                        <p>Sobre o Palestrante</p>
+                    </div>
+                    <div class="content">
+                        <h4>Título da Palestra: com subtítulo talvez bem grande</h4>
+                        <p>Nome Sobrenome</p>
+                        <p>Texto resumo da palestra, apelativo, okay? Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+                </div>
+            </div>            
+        </section>
+
+        <section class="container-fluid content-testimony">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 col-xs-12">
+                    <blockquote>
+                            Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat.
+                            <p class="text-right"><i>Nome Sobrenome</i></p>
+                    </blockquote>
+                </div>
+            </div>
+        </section>
+
+        <section class="container content-partners">
+            <h2 class="row text-center text-uppercase">Patrocinadores e Parceiros</h2>
+
+            <p>&nbsp;</p>
+            
+            <div class="row">
+                <div class="col-lg-offset-3 col-lg-2 col-md-offset-3 col-md-2 col-sm-12 text-center">
+                    <div class="profile">
+                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 text-center">
+                    <div class="profile">
+                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-2 col-sm-2 text-center">
+                    <div class="profile">
+                        <img class="img-responsive img-thumbnail" src="http://via.placeholder.com/150x150"/>
+                    </div>
+                </div>
+                
+            </div>        
+        </section>
+
+        <section class="container-fluid content-ticket">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8 col-xs-12">
+                    <h2>Não perca essa oportunidade</h2>
+                    <p>
+                        Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Nulla porttitor accumsan tincidunt. Cras ultricies ligula sed magna dictum porta.
+                    </p>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-12">
+                    <a href="https://www.sympla.com.br/talks-inbound-marketing--ux-design--business__193893" target="_blank" class="btn btn-primary btn-lg btn-ticket text-uppercase">Garanta seu ingresso</a>
+                </div>
+            </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="container">
+            <p>&nbsp;</p>
+            <div class="row contact">
+                <p>Dúvidas? Entre em contato conosco!</p>
+                <p>email@email.com</p>
+                <p>(48) 9 9999 9999</p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
