6372fa6afcbe8a77ffc9186538ee64ddbb70269d
Alexandro Feijó <desenvolvimento@cenazero.com>
2017-09-29 21:13:51 -0300

    pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,32 @@
-<!DOCTYPE html>
-
 <html>
 
 <head>
-    
-    <!-- Global Site Tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107081638-2"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments)};
-      gtag('js', new Date());
-
-      gtag('config', 'UA-107081638-2');
-    </script>
 
     <!-- Required meta tags -->
     <meta charset="utf-8">
 
-    <title>MKT Improvável: Inbound Marketing onde ninguém imagina</title>
+    <title>Estudo de Caso: Como a Nike e a Adidas usaram o poder dos aplicativos para se aproximar de seus clientes</title>
+<!--
+    <link rel="icon" type="image/png" href="img/favicon_16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="img/favicon_32x32.png" sizes="32x32" />
 
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_16x16.png')}}" sizes="16x16" />
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon_32x32.png')}}" sizes="32x32" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
-    <meta name="description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias" />
+    <meta name="description" content="Leia o estudo de caso que a Mobile Growth preparou sobre como a Nike e a Adidas vêm encantando e se relacionando com seus cliente através de aplicativos" />
 
-    <meta property="og:url" content="https://mktimprovavel.herokuapp.com/" />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="MKT Improvável: Inbound Marketing onde ninguém imagina" />
-    <meta property="og:description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias" />
-    <meta property="og:image" content="{{ asset('assets/img/cover_FB3.jpg')}}" />
-
+    <meta property="og:url" content="http://mobilegrowth.com.br{% url 'lp_caseadidasnike" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Estudo de Caso: Como a Nike e a Adidas usaram o poder dos aplicativos para se aproximar de seus clientes" />
+    <meta property="og:description" content="Leia o estudo de caso que a Mobile Growth preparou sobre como a Nike e a Adidas vêm encantando e se relacionando com seus cliente através de aplicativos" />
+    <meta property="og:image" content="http://mobilegrowth.com.brimg/p05-nike-snkrs01.jpg" />
+    
     <meta name="twitter:card" content="summary">
-    <meta name="twitter:url" content="https://mktimprovavel.herokuapp.com/">
-    <meta name="twitter:title" content="MKT Improvável: Inbound Marketing onde ninguém imagina">
-    <meta name="twitter:description" content="Como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias">
-    <meta name="twitter:image" content="{{ asset('assets/img/cover_FB3.jpg')}}">
+    <meta name="twitter:url" content="http://mobilegrowth.com.br{% url 'lp_caseadidasnike">
+    <meta name="twitter:title" content="Estudo de Caso: Como a Nike e a Adidas usaram o poder dos aplicativos para se aproximar de seus clientes">
+    <meta name="twitter:description" content="Leia o estudo de caso que a Mobile Growth preparou sobre como a Nike e a Adidas vêm encantando e se relacionando com seus cliente através de aplicativos">
+    <meta name="twitter:image" content="http://mobilegrowth.com.brimg/p05-nike-snkrs01.jpg">
+-->
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
@@ -47,120 +37,119 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <script src="{{ asset('assets/js/script.js')}}"></script>
-
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,800,800i|Open+Sans:400,400i,700,700i" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
+
+    <script src="{{ asset('assets/js/main.js')}}"></script>
 
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
 
 </head>
 
-<body>
-    <header class="jumbotron cover">
+<body class="landing-page">
+    <header class="cover">
         <div class="row">
-            <div class="col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-lg-6 col-md-6 col-sm-6 col-xs-12 title">
-                <h1>Marketing Improvável</h1>
-                <h3>Inbound Marketing onde ninguém imagina</h3>
-                <p>&nbsp;</p>
-                <p>Como ONGs, universidades e governos estão praticando <b>Inbound Marketing</b> para atrair pessoas e fechar parcerias</p>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 date-location">
-                <h3>27 Setembro</h3>
-                <h3>19h - 21h</h3>
-                <h3>Florianópolis</h3>
-            </div>
-        </div>
-        <div class="row cover-cta">
-            <a class="btn btn-primary btn-lg text-uppercase" href="https://www.sympla.com.br/mkt-improvavel-inbound-marketing-onde-ninguem-imagina__193893">Garanta sua Vaga</a>
+            
+                <img class="brand img-responsive" src="{{ asset('assets/img/brand.png')}}" />
+
+                <h1>Go Good | Pessoas e Resultados</h1>
+            
         </div>
     </header>
-    
     <main>
-        <section class="container content-talks">
-            <h2 class="row text-center text-uppercase">Quem vem?</h2>
-            <p>&nbsp;</p>
+        <div class="container section01">   
             <div class="row">
-                <div class="col-md-offset-2 col-md-8 card-talk">
-                    <div class="profile text-center">
-                        <img class="img-circle" src="{{ asset('assets/img/michelle.png')}}"/>
-                        <p class="speaker-name">Michelle Rocha<br />Politize!</p>
-                    </div>
-                    <div class="content">
-                        <h4>Atração de Parceiros por meio de Inbound Marketing na Politize!</h4>
-                        <p class="speaker-name">Michelle Rocha de Moura, Designer Gráfica na Politize!</p>
-                        <p>A Politize! é uma rede de pessoas e organizações comprometidas com a ideia de levar educação política para cidadãos de todo o Brasil. A aplicação estratégica de Inbound Marketing nos canais de comunicação da Politize! é fundamental para atrair e manter parceiros apoiadores. Vamos falar um pouco sobre como foi estruturar estas práticas e o que alcançamos até então.</p>
-                    </div>
-                </div>
-                <div class="col-md-offset-2 col-md-8 card-talk">
-                    <div class="profile text-center">
-                        <img class="img-circle" src="{{ asset('assets/img/profile_pic.png')}}"/>
-                    </div>
-                    <div class="content">
-                        <h4>A definir</h4>
-                    </div>
-                </div>
-            </div>            
-        </section>
+                <div class="col-md-8 col-sm-7">
+                    <h2 class="asset-title">GO GOOD: uma experiência estimulante para sua empresa e colaboradores</h2>
 
-        <section class="container-fluid content-testimony">
-            <h2 class="row text-center text-uppercase">Sobre</h2>
-            <div class="row">
-                <div class="col-sm-4 col-sm-offset-2 col-md-4 col-md-offset-2 col-lg-4 col-lg-offset-2 col-xs-12">
-                    <p>Convidamos a Politize! para conversar sobre como ONGs, universidades e governos estão praticando Inbound Marketing para atrair pessoas e fechar parcerias.
-                    </p>
-                    <p>O Inbound Marketing ou Marketing Digital tem atraído empresas de todos os tamanhos como estratégia para conversar com potenciais clientes e conseguir, ao mesmo tempo, maior assertividade e otimização de recursos no momento de vender seus produtos e serviços. Quem já não ouviu falar em funil de vendas? Em landing pages e ebooks para captar leads???</p>
+                    <p>A prática de hábitos saudáveis e de atividades físicas não gera apenas vantagens para o funcionário, sua empresa também pode ganhar com o bem- estar físico, mental e espiritual do seu colaborador.</p>
+
+                    <p>Revolucione os hábitos dos seus colaboradores.</p>
+
+                    <p>Conheça nossa espetacular ferramenta que é capaz de:</p>
+
+                    <ul>
+                        <li>Impulsionar a performance dos seus colaboradores;</li>
+                        <li>Reduzir a falta dos seus colaboradores por motivos de saúde;</li>
+                        <li>Ampliar a visibilidade das ações sociais de sua empresa.</li>
+                    </ul>
+
+                    <p><b>Me pergunte como!</b> Basta preencher o formulário <span class="hidden-xs">ao lado</span><span class="hidden-sm hidden-md hidden-lg">abaixo</span>.</p>
                 </div>
-                <div class="col-sm-4 col-md-4 col-lg-4 col-xs-12">
-                    <p>Para empresas que visam lucro é mais do que natural praticar Inbound Marketing. Será que ONGs conseguem aplicar os mesmo métodos, as mesmas práticas? Com que objetivo? Convidamos a Michelle Rocha, designer gráfica na Politize!, para compartilhar experiências na utilização de marketing digital de conteúdo nos canais de comunicação deles.</p>
+                <div class="col-md-offset-1 col-md-3 col-sm-offset-1 col-sm-4 form-newsletter">
+                    <form method="post">
+          
+                        <div>
+                            <p class="title">Cadastre-se e assista o nosso webinar</p>
+
+                            <div>
+                                <label for="id_first_name">Nome</label>
+                                <input class="form-control" type="text" name="first_name" required id="id_first_name" maxlength="100" />
+                            </div>
+                            <div >
+                                <label for="id_last_name">Sobrenome</label>
+                                <input class="form-control" type="text" name="last_name" required id="id_last_name" maxlength="100" />
+                            </div>
+                            <div >
+                                <label for="id_email">E-mail Corporativo</label>
+                                <input class="form-control" type="email" name="email" required id="id_email" maxlength="254" />
+                            </div>
+                            <div>
+                                <label for="id_company">Empresa</label>
+                                <input class="form-control" type="text" name="company" required id="id_company" maxlength="254" />
+                            </div>
+                            <div>
+                                <label for="id_numbers">Quantidade de Funcionários</label>
+                                <br />
+                                <select class="form-control" name="numbers" required id="id_numbers" >
+                                    <option value="op01">0-50</option>
+                                    <option value="op02">51-100</option>
+                                    <option value="op03">101-1000</option>
+                                    <option value="op04">1001-2000</option>
+                                    <option value="op05">Mais de 2001</option>
+                                </select>
+                                
+                            </div>
+
+                            <div >
+                                <input style="margin-top: 20px;" class="btn btn-default btn-primary center-block" type="submit" value="Me Pergunte Como" />
+                            </div>
+                            <p class="help-msg">Prometemos não utilizar suas informações de contato para enviar qualquer tipo de SPAM.</p>
+                        </div>
+                    </form>
                 </div>
             </div>
-        </section>
-
-        <section class="container content-partners">
-            <h2 class="row text-center text-uppercase">Parceiros e Apoiadores</h2>
-
-            <p>&nbsp;</p>
+            
+        </div>
+        <div class="container-fluid section02">
+            <div class="row">
+                <h2 class="text-center">Veja quem está usando Go Good</h2>
+            </div>
+            <div class="row">
+                <section class="col-lg-offset-2 col-lg-8 col-md-8 col-md-offset-2 col-sm-offset-2 col-sm-8">
+                    <img class="img-responsive center-block" src="img/companies.png" >
+                </section>
+                <section class="col-lg-offset-2 col-lg-8 col-md-8 col-md-offset-2 col-sm-offset-2 col-sm-8">
+                    
+                </section>
+            </div>
+        </div>
+        <!--
+        <div class="container-fluid section03">
             
             <div class="row">
-                <div class="col-lg-offset-3 col-lg-3 col-md-offset-3 col-md-3 col-sm-12 text-center">
-                    <div class="profile">
-                        <a href="http://www.politize.com.br/" target="_blank"><img class="img-responsive img-thumbnail" src="{{ asset('assets/img/logotipo-header-politize1_small.png')}}"/></a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-12 text-center">
-                    <div class="profile">
-                        <a href="https://www.vilaj.com.br/" target="_blank"><img class="img-responsive img-thumbnail" src="{{ asset('assets/img/vilaj_brand.png')}}"/></a>
-                    </div>
-                </div>                
-            </div>        
-        </section>
-
-        <section class="container-fluid content-ticket">
-            <div class="container">
-            <div class="row">
-                <div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-offset-2 col-sm-8 col-xs-12">
-                    <h2>Não perca essa oportunidade!</h2>
-                    <p>
-                        Conheça experiências inovadoras na aplicação de <b>Inbound Marketing</b> em iniciativas sem fins lucrativos.
-                    </p>
-                </div>
+                <section class="col-lg-8 col-md-8 col-sm-8">
+                    
+                </section>
             </div>
-            <div class="row text-center">
-                <div class="col-md-12">
-                    <a href="https://www.sympla.com.br/mkt-improvavel-inbound-marketing-onde-ninguem-imagina__193893" target="_blank" class="btn btn-primary btn-lg btn-ticket text-uppercase">Garanta seu ingresso</a>
-                </div>
-            </div>
-            </div>
-        </section>
+        </div>
+    -->
     </main>
-
-    <footer>
+    <footer >
         <div class="container">
-            <div class="row contact">
-                <p>Dúvidas? Entre em contato conosco!</p>
-                <p>andrea.bonette.ferrari@gmail.com</p>
-                <p>(48) 9 9646 5382</p>
+            
+            <div class="row copyright">
+                <p>Go Good | Pessoas e Resultados. Todos os direito reservados. Setembro, 2017.</p>
             </div>
         </div>
     </footer>
@@ -173,8 +162,7 @@
                 .build();
         }
     })();
-</script>        
-            
+</script>
 </body>
 
 </html>
